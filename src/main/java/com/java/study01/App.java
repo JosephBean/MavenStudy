be62ef0a0d0371cc280e828445d3c95deeb6f01f
JosephBean <kk1593@naver.com>
2024-12-20 16:17:14 +0900

    mybatis settings

--- src/main/java/com/java/study01/App.java
+++ src/main/java/com/java/study01/App.java
@@ -1,17 +1,28 @@
 package com.java.study01;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 public class App {
 
 	public static void main(String[] args) {
-		String driver = "org.mariadb.jdbc.Driver";
 		try {
-			System.out.println("JDBC Start!");
-			Class.forName(driver);
-		} catch (ClassNotFoundException e) {
+			String resource = "mybatis-config.xml";
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			System.out.println("Factory Connection!!");
 
-		} catch (Exception e) {
+			SqlSession session = sqlSessionFactory.openSession();
+			int result =  session.selectOne("test.test1");
+			System.out.println("Result : " + result);
+		} catch (IOException e) {
 			e.printStackTrace();
-		} 
+		}
 	}
 
 }
